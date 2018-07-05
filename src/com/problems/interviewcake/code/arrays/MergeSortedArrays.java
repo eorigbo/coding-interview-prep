1def656dc8326580fd610b79c8faa293afd9b922
Efesa Origbo <efeori@microsoft.com>
2018-07-05 08:00:59 -0700

    added code and test for MergeSortedArrays

---
+++ src/com/problems/interviewcake/code/arrays/MergeSortedArrays.java
@@ -0,0 +1,39 @@
+package com.problems.interviewcake.code.arrays;
+
+public class MergeSortedArrays {
+
+    public static int[] mergeArrays(int[] firstArray, int[] secondArray){
+
+        //setup our mergedArray
+        int[] mergedArray = new int[firstArray.length + secondArray.length];
+
+        int currentIndexFirst = 0;
+        int currentIndexSecond = 0;
+        int currentIndexMerged = 0;
+
+        while (currentIndexMerged < mergedArray.length){
+
+            boolean isFirstArrayExhausted = currentIndexFirst >= firstArray.length;
+            boolean isSecondArrayExhausted = currentIndexSecond >= secondArray.length;
+
+            /*
+            add firstArray element to mergedArray AND
+            increment currentIndexFirst if
+            first array is not exhausted AND
+            second array is exhausted OR
+            currentIndexFirst < currentIndexSecond
+            */
+            if (!isFirstArrayExhausted && (isSecondArrayExhausted || firstArray[currentIndexFirst] < secondArray[currentIndexSecond])){
+                mergedArray[currentIndexMerged] = firstArray[currentIndexFirst];
+                currentIndexFirst++;
+            }else{
+                mergedArray[currentIndexMerged] = secondArray[currentIndexSecond];
+                currentIndexSecond++;
+            }
+
+            currentIndexMerged++;
+        }
+
+        return mergedArray;
+    }
+}
---
+++ src/com/problems/interviewcake/test/arrays/MergeSortedArraysTest.java
@@ -0,0 +1,48 @@
+package com.problems.interviewcake.test.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.problems.interviewcake.code.arrays.MergeSortedArrays.mergeArrays;
+
+public class MergeSortedArraysTest {
+
+    @Test
+    public void mergeArraysTest(){
+        int[] firstArray = new int[]{3,4,6,10,11,15};
+        int[] secondArray = new int[]{1,5,8,12,14,19};
+
+        int[] mergedArray = mergeArrays(firstArray, secondArray);
+
+        int expectedMergedArraySize = 12;
+        int actualMergedArraySize = mergedArray.length;
+
+        Assertions.assertEquals(expectedMergedArraySize,actualMergedArraySize);
+    }
+
+    @Test
+    public void mergeArraysTestFirst(){
+        int[] firstArray = new int[]{3,4,6,10,11,15};
+        int[] secondArray = new int[]{1,5,8,12,14,19};
+
+        int[] mergedArray = mergeArrays(firstArray, secondArray);
+
+        int expectedMergedFirstElement = 1;
+        int actualMergedFirstElement = mergedArray[0];
+
+        Assertions.assertEquals(expectedMergedFirstElement,actualMergedFirstElement);
+    }
+
+    @Test
+    public void mergeArraysTestLast(){
+        int[] firstArray = new int[]{3,4,6,10,11,15};
+        int[] secondArray = new int[]{1,5,8,12,14,19};
+
+        int[] mergedArray = mergeArrays(firstArray, secondArray);
+
+        int expectedMergedLastElement = 19;
+        int actualMergedLastElement = mergedArray[mergedArray.length-1];
+
+        Assertions.assertEquals(expectedMergedLastElement,actualMergedLastElement);
+    }
+}
